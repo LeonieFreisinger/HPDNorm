658521f8cd503adc12219ccfe0756515f3b9e61a
LeonieFreisinger <ge79kej@mytum.de>
2023-08-09 05:02:04 +0000

    rebuild EXE_CHARACTERIZE

--- EXE_CHARACTERIZE.py
+++ EXE_CHARACTERIZE.py
@@ -1,5 +1,5 @@
-from evaluation.characteristics.feature_analysis import exe_calulate_features_synthetic_data, exe_calulate_features_real_world_data
+from evaluation.characteristics.feature_analysis import exe_calulate_features_real_world_data, exe_calulate_features_synthetic_data
 
 if __name__ == "__main__":
-    # exe_calulate_features_synthetic_data()
+    exe_calulate_features_synthetic_data()
     exe_calulate_features_real_world_data()
