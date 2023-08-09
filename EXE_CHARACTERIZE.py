3b02e37ca2e8b1b92b124def0d668e66535e1b50
LeonieFreisinger <76744817+LeonieFreisinger@users.noreply.github.com>
2023-08-09 07:14:11 +0200

    Delete EXE_CHARACTERIZE.py

--- EXE_CHARACTERIZE.py
+++
@@ -1,5 +0,0 @@
-from evaluation.characteristics.feature_analysis import exe_calulate_features_synthetic_data, exe_calulate_features_real_world_data
-
-if __name__ == "__main__":
-    # exe_calulate_features_synthetic_data()
-    exe_calulate_features_real_world_data()
