b280261edb9520373003fbbdfdb8ca785af8a943
LeonieFreisinger <ge79kej@mytum.de>
2023-09-28 15:22:58 +0200

    change requirements

--- installation.sh
+++ installation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pip install -r requirements.txt
-pip install -e git+https://github.com/ourownstory/test-of-time.git@evolution_experiments_2#egg=test-of-time
+pip install git+https://github.com/ourownstory/test-of-time.git
+#pip install -e git+https://github.com/ourownstory/test-of-time.git@evolution_experiments_2#egg=test-of-time
 pip install git+https://github.com/ourownstory/neural_prophet.git@normalization-layer
-pip install git+https://github.com/LeonieFreisinger/darts.git@lgbm_for_server#egg=darts
+#pip install git+https://github.com/LeonieFreisinger/darts.git@lgbm_for_server#egg=darts
