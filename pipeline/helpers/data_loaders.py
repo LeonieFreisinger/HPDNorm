b1aff0b41e1de4dcd5bad2bc9947d9749f33aafc
LeonieFreisinger <76744817+LeonieFreisinger@users.noreply.github.com>
2023-11-25 10:15:00 +0100

    Implement datasets download (#3)
    * add download from huggingface datasets
    * adapt split='all' in data loader
    * fix warning group_keys = False

--- pipeline/helpers/data_loaders.py
+++ pipeline/helpers/data_loaders.py
@@ -5,9 +5,11 @@ from pathlib import Path
 import pandas as pd
 from tot.error_utils import raise_if
 
+from datasets import load_dataset
 
 DATA_DIR = os.path.join(Path(__file__).parent.parent.parent.absolute(), "datasets")
 
+# datasets are loaded from huggingface datasets if not already present in the data folder. Login required.
 def load(path, n_samples=None, ids=None, n_ids=None):
     df = pd.read_csv(path)
 
@@ -23,7 +25,7 @@ def load(path, n_samples=None, ids=None, n_ids=None):
         df = df[df["ID"].isin(ids)].reset_index(drop=True)
     if n_samples is not None:
         df = (
-            df.groupby("ID")
+            df.groupby("ID", group_keys=False)
             .apply(lambda x: x.iloc[:n_samples, :].copy(deep=True))
             .reset_index(drop=True)
         )
@@ -31,8 +33,13 @@ def load(path, n_samples=None, ids=None, n_ids=None):
 
 
 def load_EIA():
+    destination = DATA_DIR + "/eia_electricity_hourly.csv"
+    if not os.path.exists(destination):
+        dataset = load_dataset("HPDNorm/eia_electricity_hourly", split="all")
+        dataset.to_csv(destination, index=None)
+
     return load(
-        DATA_DIR + "/eia_electricity_hourly.csv",
+        destination,
         ids=[
             "AEC",
             "AECI",
@@ -50,33 +57,74 @@ def load_EIA():
 
 
 def load_London():
+    destination = DATA_DIR + "/london_electricity_hourly.csv"
+    if not os.path.exists(destination):
+        dataset = load_dataset("HPDNorm/london_electricity_hourly", split="all")
+        dataset.to_csv(destination, index=None)
+
     return load(
-        DATA_DIR + "/london_electricity_hourly.csv",
+        destination,
         ids=["T1", "T33", "T67", "T89", "T122", "T156", "T180", "T209", "T281", "T307"],
         n_samples=26280,
     )
 
 
 def load_ERCOT():
-    return load(DATA_DIR + "/ercot_load_reduced.csv")
+    destination = DATA_DIR + "/ercot_load_reduced.csv"
+    if not os.path.exists(destination):
+        dataset = load_dataset("HPDNorm/ercot_load_reduced", split="all")
+        dataset.to_csv(destination, index=None)
+
+    return load(destination)
 
 
 def load_Australian():
-    return load(
-        DATA_DIR + "/australian_electricity_half_hourly.csv", n_ids=5, n_samples=52560
-    )
+    destination = DATA_DIR + "/australian_electricity_half_hourly.csv"
+    if not os.path.exists(destination):
+        dataset = load_dataset("HPDNorm/australian_electricity_half_hourly", split="all")
+        dataset.to_csv(destination, index=None)
+
+    return load(destination, n_ids=5, n_samples=52560)
 
 
 def load_Solar():
+    destination = DATA_DIR + "/solar_10_minutes_dataset.csv"
+    if not os.path.exists(destination):
+        dataset = load_dataset("HPDNorm/solar_10_minutes_dataset", split="all")
+        dataset.to_csv(destination, index=None)
+
     return load(
-        DATA_DIR + "/solar_10_minutes_dataset.csv",
+        destination,
         ids=["T1", "T19", "T34", "T59", "T66", "T83", "T91", "T111", "T124", "T134"],
         n_samples=26280,
     )
 
 
 def load_ETTh():
-    return load(DATA_DIR + "/ETTh_panel.csv", n_ids=14, n_samples=26280)
+    destination = DATA_DIR + "/ETTh_panel.csv"
+    if not os.path.exists(destination):
+        dataset = load_dataset("HPDNorm/ETTh_panel", split="all")
+        dataset.to_csv(destination, index=None)
+
+    return load(destination, n_ids=14, n_samples=26280)
+
+
+def load_WebTraffic():
+    destination = DATA_DIR + "/kaggle_web_traffic_1000.csv"
+    if not os.path.exists(destination):
+        dataset = load_dataset("HPDNorm/kaggle_web_traffic_1000", split="all")
+        dataset.to_csv(destination, index=None)
+
+    return load(destination)
+
+
+def load_M5():
+    destination = DATA_DIR + "/m5_aggregated_3049.csv"
+    if not os.path.exists(destination):
+        dataset = load_dataset("HPDNorm/m5_aggregated_3049", split="all")
+        dataset.to_csv(destination, index=None)
+
+    return load(destination)
 
 
 DATASETS = {
@@ -86,4 +134,6 @@ DATASETS = {
     "Australian": {"load": load_Australian, "freq": "30min"},
     "Solar": {"load": load_Solar, "freq": "10min"},
     "ETTh": {"load": load_ETTh, "freq": "H"},
+    "WebTraffic": {"load": load_WebTraffic, "freq": "D"},
+    "M5": {"load": load_M5, "freq": "D"},
 }
