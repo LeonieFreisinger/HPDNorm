d07daa2b89107f61707c2823c5dcef70d2951f0c
LeonieFreisinger <ge79kej@mytum.de>
2023-11-25 10:42:40 +0100

    add comments

--- installation.sh
+++ installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pip install -r requirements.txt
-pip install git+https://github.com/ourownstory/neural_prophet.git@normalization-layer
+pip install git+https://github.com/ourownstory/neural_prophet.git@normalization-layer # install neuralprophet first to have the right torch version
 pip install git+https://github.com/LeonieFreisinger/darts.git@master
-pip install git+https://github.com/ourownstory/test-of-time.git
+pip install git+https://github.com/ourownstory/test-of-time.git # install test-of-time last to have the right neuralprophet and darts version
